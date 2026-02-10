cf9337a159cbb17dcef07a25f74269b3c4d90a33
Piyush-WD <gupta.piyush1005@gmail.com>
2026-02-10 23:44:04 +0530

    Add solution for Unique Paths

--- LeetCode/Unique-Paths/Solution.cpp
+++ LeetCode/Unique-Paths/Solution.cpp
@@ -1,19 +1,22 @@
-class Solution {
-public:
-    int uniquePaths(int m, int n) {
-        vector<vector<int>> grid(m, vector<int>(n));
-        for(int i=0;i<n;i++){
-            grid[0][i]=1;
-        }
-        for(int i=1;i<m;i++){
-            grid[i][0]=1;
-        }
-        for(int i=1;i<m;i++){
-            for(int j=1;j<n;j++){
-                grid[i][j]=grid[i-1][j]+grid[i][j-1];
-            }
-        }
-        // grid[m-1][n-1]=grid[m-2][n-1]+grid[m-1][n-2];
-        return grid[m-1][n-1];
-    }
-};
+1class Solution {
+2public:
+3    int solve(int i, int j, vector<vector<int>>&dp, int m, int n){
+4        if(i==m-1&&j==n-1){
+5            return 1;
+6        }
+7        if(i>=m||j>=n){
+8            return 0;
+9        }
+10        if(dp[i][j]!=-1){
+11            return dp[i][j];
+12        }
+13        int right = solve(i,j+1,dp,m,n);
+14        int down = solve(i+1,j,dp,m,n);
+15        return dp[i][j]=right+down;
+16    }
+17
+18    int uniquePaths(int m, int n) {
+19        vector<vector<int>>dp(m, vector<int>(n, -1));
+20        return solve(0, 0, dp, m, n);
+21    }
+22};
